b4a7988e46438cfc1c1f9966800f5b012dd11e6d
thahamh <32389972+thahamh@users.noreply.github.com>
2018-03-26 15:33:50 +0800

    Membuat Atribut dan Behavior

--- src/praktek1/Minuman.java
+++ src/praktek1/Minuman.java
@@ -10,5 +10,19 @@ package praktek1;
  * @author ASUS X456U
  */
 public class Minuman {
+    string warna;
+    string kemasan;
+    string jumlah_isi;
+    string warna_kemasan;
+    string harga;
     
+    
+    void cetakInfo(){
+        system.out.prinln("Warna            :"+warna);
+        system.out.prinln("Kemasan          :"+kemasan);
+        system.out.prinln("Jumlah_isi       :"+jumlah_isi);
+        system.out.prinln("warna_Kemasan    :"+warna_kemasan);
+        system.out.prinln("Harga            :"+harga);
+        
+    }
 }
